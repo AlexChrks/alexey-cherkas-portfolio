58d27508026046a33527413ef8956c5ae8ebc273
Alexey Cherkas <alexey.cherkas1997@gmail.com>
2021-06-16 14:21:33 +0300

    refactor: change works links

--- portfolio/constants/works.js
+++ portfolio/constants/works.js
@@ -60,7 +60,7 @@ const works = [
     timeInterval: 'May 2021 - May 2021',
     title: 'Weather Forecast',
     description: 'Simple weather-forecast application built with React.js and WeatherAPI. Beauty, useability, convenience',
-    deployLink: '#'
+    deployLink: '/works'
   },
   {
     id: 7,
@@ -69,7 +69,7 @@ const works = [
     timeInterval: 'May 2021 - June 2021',
     title: 'Currency exchange',
     description: 'Useful currency exchange application built with pure React helps to control market rates.',
-    deployLink: '#'
+    deployLink: '/works'
   },
 ];
 
