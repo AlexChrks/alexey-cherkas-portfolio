e7293157bb3684b68249f2110018de5fd50fb1a8
Alexey Cherkas <alexey.cherkas1997@gmail.com>
2021-06-30 16:26:27 +0300

    feat: change clickable work area

--- portfolio/components/WorksContent/WorksItem/WorksItem.js
+++ portfolio/components/WorksContent/WorksItem/WorksItem.js
@@ -5,20 +5,20 @@ import styles from './WorksItem.module.scss';
 function WorksItem({id, imageSrc, technologies, timeInterval, title}) {
 
   return (
-    <div className={styles.itemWrapper}>
+    <Link href={`/project/${id}`} className={styles.item}> 
+      <div className={styles.itemWrapper}>
 
-      <div className={styles.imageContainer}>
-        <img className={styles.image} src={imageSrc}/>
-        <div className={styles.technologies}>{technologies}</div>
+        <div className={styles.imageContainer}>
+          <img className={styles.image} src={imageSrc}/>
+          <div className={styles.technologies}>{technologies}</div>
+        </div>
+
+        <span className={styles.timeInterval}>{timeInterval}</span>
+        <h3>{title}</h3>
+        
+          <p>Read more</p> 
       </div>
-
-      <span className={styles.timeInterval}>{timeInterval}</span>
-      <h3>{title}</h3>
-      <Link href={`/project/${id}`}>
-        <p>Read more</p>
-      </Link>
-      
-    </div>
+    </Link>
   )
 }
 
